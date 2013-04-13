59d140893853aaf1a616ab5e73c81dffa8f0c10b
Thomas Deinhamer <thasmo@gmail.com>
2013-04-13 22:13:01 +0200

    Remove unneeded configuration file for igbinary

--- .chef/cookbook/_box/recipes/module_igbinary.rb
+++ .chef/cookbook/_box/recipes/module_igbinary.rb
@@ -2,17 +2,6 @@ php_pear "igbinary" do
   action :install
 end
 
-cookbook_file "/etc/php5/mods-available/igbinary.ini" do
-  source "igbinary.ini"
-  mode 0644
-  owner "root"
-  group "root"
-end
-
-link '/etc/php5/conf.d/20-igbinary.ini' do
-  to '../mods-available/igbinary.ini'
-end
-
 service "php5-fpm" do
   action :restart
 end
