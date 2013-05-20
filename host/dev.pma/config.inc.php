b97508b7751fc6b7bb89960a665e81f247b2650f
Thomas Deinhamer <thasmo@gmail.com>
2013-05-20 17:13:34 +0200

    Add phing installation script for phpmyadmin

---
+++ host/dev.pma/config.inc.php
@@ -0,0 +1,28 @@
+<?php
+
+$cfg['DefaultLang'] = 'de';
+$cfg['ServerDefault'] = 1;
+$cfg['UploadDir'] = '';
+$cfg['SaveDir'] = '';
+$cfg['CheckConfigurationPermissions'] = false;
+$cfg['PmaNoRelation_DisableWarning'] = true;
+$cfg['McryptDisableWarning'] = true;
+$cfg['UserprefsDeveloperTab'] = true;
+$cfg['Error_Handler']['display'] = true;
+$cfg['NumRecentTables'] = 100;
+
+/* Server: localhost [1] */
+$cfg['Servers'][1]['verbose'] = 'localhost';
+$cfg['Servers'][1]['host'] = 'localhost';
+$cfg['Servers'][1]['port'] = '';
+$cfg['Servers'][1]['socket'] = '';
+$cfg['Servers'][1]['connect_type'] = 'tcp';
+$cfg['Servers'][1]['extension'] = 'mysqli';
+$cfg['Servers'][1]['nopassword'] = true;
+$cfg['Servers'][1]['auth_type'] = 'config';
+$cfg['Servers'][1]['user'] = 'root';
+$cfg['Servers'][1]['password'] = '';
+$cfg['Servers'][1]['hide_db'] = '(information_schema|performance_schema|phpmyadmin|mysql|test)';
+$cfg['Servers'][1]['AllowNoPassword'] = true;
+
+?>
