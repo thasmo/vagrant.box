85e62a66eef998da343e17e21d9ed49624a9a824
Thomas Deinhamer <thasmo@gmail.com>
2013-05-25 23:50:22 +0200

    Add dynamic configuration of vhosts

--- .chef/cookbook/specific/base/recipes/hosts.rb
+++ .chef/cookbook/specific/base/recipes/hosts.rb
@@ -1,9 +1,34 @@
 execute "disable-default-site" do
 	command "sudo a2dissite default"
-	notifies :reload, resources(:service => "apache2"), :delayed
 end
 
-web_app "hosts" do
-	template "host.conf.erb"
-	notifies :reload, resources(:service => "apache2"), :delayed
+cookbook_file node['apache']['dir'] + "/conf.d/custom" do
+	source "custom.conf"
+	mode 00644
+	owner "root"
+	group node['apache']['root_group']
+end
+
+Dir.foreach("/vagrant/host") do |host|
+	next if /^(\.|~|_)/.match(host)
+
+	directories = [
+		"/vagrant/host/" + host + "/htdocs",
+		"/vagrant/host/" + host + "/httpdocs",
+		"/vagrant/host/" + host + "/public",
+	]
+
+	directories.each { |directory|
+
+		if Dir.exist?(directory)
+			web_app host do
+				template "host.conf.erb"
+				server_name host + ".local"
+				docroot directory
+				allow_override "All"
+			end
+
+			break
+		end
+	}
 end
