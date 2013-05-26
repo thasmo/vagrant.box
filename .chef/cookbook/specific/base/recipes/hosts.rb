52d1d2b9f5fdc22f238ace90e39e374501e6911a
Thomas Deinhamer <thasmo@gmail.com>
2013-05-26 23:06:33 +0200

    Add support for subdomains

--- .chef/cookbook/specific/base/recipes/hosts.rb
+++ .chef/cookbook/specific/base/recipes/hosts.rb
@@ -6,6 +6,14 @@ execute "remove-other-vhosts-access-logs" do
   command "sudo rm -f /etc/apache2/conf.d/other-vhosts-access-log"
 end
 
+execute "remove-all-enabled-sites" do
+  command "sudo rm -f /etc/apache2/sites-enabled/*"
+end
+
+execute "remove-all-available-sites" do
+  command "sudo rm -f /etc/apache2/sites-available/*"
+end
+
 cookbook_file node['apache']['dir'] + "/conf.d/custom" do
   source "custom.conf"
   mode 00644
@@ -25,7 +33,7 @@ Dir.foreach("/var/www") do |host|
   directories.each { |directory|
 
     if Dir.exist?(directory)
-      web_app host do
+      web_app "1-" + host do
         template "host.conf.erb"
         server_name host + ".local"
         docroot directory
@@ -35,4 +43,32 @@ Dir.foreach("/var/www") do |host|
       break
     end
   }
+
+  subdirectory = "/var/www/" + host + "/sub"
+
+  if Dir.exist?(subdirectory)
+    Dir.foreach(subdirectory) do |subhost|
+      next if /^(\.|~|_)/.match(subhost)
+
+      subdirectories = [
+        subdirectory + "/" + subhost + "/htdocs",
+        subdirectory + "/" + subhost + "/httpdocs",
+        subdirectory + "/" + subhost + "/public",
+      ]
+
+      subdirectories.each { |directory|
+
+        if Dir.exist?(directory)
+          web_app "0-" + subhost + "." + host do
+            template "host.conf.erb"
+            server_name subhost + "." + host + ".local"
+            docroot directory
+            allow_override "All"
+          end
+
+          break
+        end
+      }
+    end
+  end
 end
