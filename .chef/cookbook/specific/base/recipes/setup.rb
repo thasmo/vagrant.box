7343ffc90304ab8dc214e0266e92494f85413be3
Thomas Deinhamer <thasmo@gmail.com>
2013-06-11 01:44:15 +0200

    Reorder provisioning to properly work with the custom php.ini

--- .chef/cookbook/specific/base/recipes/setup.rb
+++ .chef/cookbook/specific/base/recipes/setup.rb
@@ -1,10 +1,3 @@
 execute "Change shell to ZSH" do
   command "sudo chsh -s $(which zsh) vagrant"
 end
-
-cookbook_file "/etc/php5/conf.d/00-custom.ini" do
-  source "php-custom.ini"
-  mode 0755
-  owner "root"
-  group "root"
-end
