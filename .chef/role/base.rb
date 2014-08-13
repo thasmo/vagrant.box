5302f1b560d1ec939102de1b2a05985a8177a0e6
Thomas Deinhamer <thasmo@gmail.com>
2014-08-13 22:05:25 +0200

    Remove ubuntu cookbook

--- .chef/role/base.rb
+++ .chef/role/base.rb
@@ -5,18 +5,9 @@ description 'Sets up a basic server'
 run_list(
 
   # Common
-  'recipe[ubuntu]',
   'recipe[locale]',
   'recipe[cron]',
   'recipe[openssl]',
   'recipe[base::ssl-cert]',
   'recipe[base::zip]'
-)
-
-# Attributes
-override_attributes(
-  'ubuntu' => {
-    'archive_url' => 'mirror://mirrors.ubuntu.com/mirrors.txt',
-    'include_source_packages' => false,
-  }
 )
