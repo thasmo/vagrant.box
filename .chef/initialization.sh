35d8c58a2c344f07725e8153d4f94a841b0f8fbd
Thomas Deinhamer <thasmo@gmail.com>
2014-10-11 21:31:07 +0200

    Update chef to version 11.16.4

--- .chef/initialization.sh
+++ .chef/initialization.sh
@@ -5,8 +5,7 @@ if [ ! -f /home/vagrant/initialization ]; then
 
   # download and install chef
   sudo wget --content-disposition https://www.opscode.com/chef/install.sh
-  sudo sh install.sh -v 11.12.8
-  sudo rm install.sh
+  sudo sh install.sh -v 11.16.4
 
   # finish initialization
   touch /home/vagrant/initialization
